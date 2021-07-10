7cb3844c69c06af19aa80dc0f6c79cceca27763d
Steven George <steven.george@hotmail.co.uk>
2021-07-10 23:56:38 +0100

    Modified mrqa squad conversion script to have answer start and text keys

--- src/data/convert_mrqa_to_squad.py
+++ src/data/convert_mrqa_to_squad.py
@@ -57,7 +57,10 @@ if __name__ == '__main__':
                             {
                                 'id': qa['qid'],
                                 'question': qa['question'],
-                                'answers': qa['answers']
+                                'answers': {
+                                    "answer_start": [x["char_spans"][0][0] for x in qa['detected_answers']],
+                                    "text": [x["text"] for x in qa['detected_answers']]
+                                }
                             } for qa in line['qas']]
                     }
                     count += len(context_dict['qas'])
