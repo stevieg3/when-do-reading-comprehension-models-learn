d9d22cacbe2ab0306611354f19dec99362c1cd26
Steven GEORGE <sgeorge@beaker.cs.ucl.ac.uk>
2021-09-04 00:27:19 +0100

    MRQA eval job gen changes

--- src/data/eval_from_file.py
+++ src/data/eval_from_file.py
@@ -2,7 +2,7 @@
 Script for generating .job files. Adapted version of script written by Max Bartolo.
 """
 
-SEEDS = [27, 29]
+SEEDS = [27, 28, 29]
 SAVE_STEPS_SCHEDULE = [1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 20, 24, 28, 32, 36, 44, 52, 60, 68, 76, 92, 108, 124, 140, 156, 172, 188, 204, 220, 236, 252, 268, 284, 300, 316, 332, 348, 364, 380, 396, 428, 460, 492, 524, 556, 588, 620, 652, 684, 716, 748, 780, 812, 844, 876, 908, 940, 972, 1004, 1036, 1100, 1164, 1228, 1292, 1356, 1420, 1484, 1548, 1612, 1676, 1804, 1932, 2060, 2188, 2316, 2444, 2572, 2700, 2828, 2956, 3084, 3212, 3340, 3468, 3596, 3724, 3852, 3980, 4108, 4236, 4364, 4492, 4620, 4748, 4876, 5004, 5132, 5260, 5388, 5516, 5644, 5772, 5900, 6028, 6156, 6284, 6412, 6540, 6668, 6796, 6924, 7052, 7180, 7308, 7436, 7564, 7692, 7820, 7948]
 
 experiments = [
@@ -83,9 +83,9 @@ if __name__ == '__main__':
 
 #$ -cwd
 #$ -S /bin/bash
-#$ -l tmem=6G
+#$ -l tmem=4G
 #$ -t 1-{len(commands)}
-#$ -l h_rt=48:00:00
+#$ -l h_rt=6:00:00
 #$ -o /SAN/intelsys/rclearn/when-do-reading-comprehension-models-learn/array_files
 #$ -e /SAN/intelsys/rclearn/when-do-reading-comprehension-models-learn/array_files
 #$ -l gpu=true
